dde072951352c87d5ca59d45834f31616c67a2c8
Jake Ledoux <contactjakeledoux@gmail.com>
2020-06-01 18:20:01 -0400

    simple --> example

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ from clem import Clem
 clem = Clem()
 
 # Load in lines
-clem.load_file("simple.clem")
+clem.load_file("example.clem")
 clem.add("casual | hi there! my name is <Jack <Black / White> \
          / Selena <Gomez>>.", "greeting")
 
